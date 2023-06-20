3515b035b694c358957b4f279ff6f0e3e44c460d
Odu Oyindamola <oduoyindamola@gmail.com>
2023-06-20 15:50:40 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 is valid (success)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'z';
+	while
+		(d >= 'd') {
+			putchar(d);
+			d--;
+		}
+	putchar('\n');
+	return (d);
+}
