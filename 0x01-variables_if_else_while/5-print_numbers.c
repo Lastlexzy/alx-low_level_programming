9c4d0e618e40402292b52682e1230d67984ea959
Odu Oyindamola <oduoyindamola@gmail.com>
2023-06-20 15:22:43 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 is valid (success)
+ */
+
+int main(void)
+{
+	int l;
+	
+	for (l = 0; a < 10; l++)
+		printf("%d", l);
+	putchar('\n');
+	return (0);
+}
