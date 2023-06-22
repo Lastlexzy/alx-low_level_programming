9925016c9148574e697396ea8ba617464c2b1f48
Odu Oyindamola <oduoyindamola@gmail.com>
2023-06-21 21:52:54 -0700

    0x02 c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
 /**
  * add - Adds two values
  * @n1: The first number to be computed
- * @n2: The secon n8umber to be computed 
+ * @n2: The secon n8umber to be computed
  * Return: Integer value
  */
 
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 	for (y = 0; y <= 9; y++)
 	{
 	z = x * y;
-	if (z >9)
+	if (z > 9)
 	{
 	u = z % 10;
 	d = (z - u) / 10;
@@ -27,7 +27,7 @@ void times_table(void)
 	}
 	else
 	{
-	if (y !=0)
+	if (y != 0)
 	{
 	_putchar(44);
 	_putchar(32);
