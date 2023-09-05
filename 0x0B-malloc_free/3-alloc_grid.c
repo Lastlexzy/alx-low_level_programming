4542106a24ea6f97c189b92526427fdb644050c1
ODU OYINDAMOLA <oduoyindamola@gmail.com>
2023-09-05 13:19:33 -0700

    malloc and free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -48,20 +48,19 @@ int **alloc_grid(int width, int height)
 
 
 
-			free(mee);
+	free(mee);
+	return (NULL);
 
-			return (NULL);
-
-		}
+	}
 
 	}
 
 
 	for (x = 0; x < height; x++)
 	{
-		for (y = 0; y < width; y++)
+	for (y = 0; y < width; y++)
 
-			mee[x][y] = 0;
+	mee[x][y] = 0;
 
 	}
 
